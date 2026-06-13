19283cf3ebabda7e932c5398d763b139e5a5b52f
Evandro Junior <evandroportesyt@gmail.com>
2026-06-12 22:38:33 -0300

    chore: create initial Spring Boot project structure

---
+++ Banco - Para facilitar o teste/banco_petshop_poo.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS petshop_poo
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE petshop_poo;
---
+++ Eclipse - Código/petshop-poo/petshop-poo/src/main/java/br/com/petshop/PetshopPooApplication.java
@@ -0,0 +1,13 @@
+package br.com.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopPooApplication.class, args);
+	}
+
+}
